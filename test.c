893f56862b71243d7abf279e73ed49c83bdd0264
valera <valera_pon@mail.ru>
2018-11-17 18:07:55 +0600

    3 test on problem A

--- test.c
+++ test.c
@@ -17,29 +17,25 @@ int set_config(int *test_count, int *check_style) {
 }
 
 int test_user_problem(char *user_program, char *way_to_test, int test_count) {
-	//int fd[2];
-	//pipe(fd);
-	//for (int i = 0; i < test_count; i++) {
+	if (fork() > 0) {
+		wait(NULL);
+	}
+	else {
+		execlp("gcc", "gcc", user_program, NULL);
+	}
+	char *path = malloc((strlen(way_to_test) + 10) * sizeof(char));
+	for (int i = 1; i <= test_count; i++) {
 		if (fork() > 0) {
 			wait(NULL);
 		}
 		else {
-			//puts("fuck");
-			//execlp("ls", "ls", NULL);
-			char *path = malloc(strlen(way_to_test) * sizeof(char) + 10);
-			sprintf(path, "%s/0%d.dat\0", way_to_test, 1);
-			if (fork() > 0) {
-				wait(NULL);
-			}
-			else {
-				//execlp("ls", "ls", NULL);
-				execlp("gcc", "gcc", user_program, NULL);
-			}
+			sprintf(path, "%s/%02d.dat\0", way_to_test, i);
 			int read_file = open(path, O_RDONLY, S_IRUSR | S_IWUSR);
 			dup2(read_file, 0);
 			execlp("./a.out", "./a.out", NULL);
 		}
-	//}
+	}
+	free(path);
 	return 0;
 }
 
@@ -48,9 +44,9 @@ int main(int argc, char **argv) {
 		perror("Not a 3 ");
 		return 1;
 	}
-	int test_count = 1, check_style = 0;
+	int test_count = 3, check_style = 0;
 	//set_config(&test_count, &check_style);
 	test_user_problem(argv[1], argv[2], test_count);
-
+	
 	return 0;
 }
